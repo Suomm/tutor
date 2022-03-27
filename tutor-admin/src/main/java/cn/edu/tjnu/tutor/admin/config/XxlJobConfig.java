2f4c9e267ba069ef84dc98729d8e3c458537ebe2
Suomm <1474983351@qq.com>
2022-03-27 10:39:44 +0800

    :heavy_plus_sign: 添加配置 xxl-job 依赖

---
+++ tutor-admin/src/main/java/cn/edu/tjnu/tutor/admin/config/XxlJobConfig.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2021-2022 the original author and authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package cn.edu.tjnu.tutor.admin.config;
+
+import cn.edu.tjnu.tutor.admin.properties.XxlJobProperties;
+import com.xxl.job.core.executor.impl.XxlJobSpringExecutor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * <a href="https://gitee.com/xuxueli0323/xxl-job/blob/master/xxl-job-executor-samples/xxl-job-executor-sample-springboot/src/main/java/com/xxl/job/executor/core/config/XxlJobConfig.java">
+ * XXL-JOB 配置信息。
+ * </a>
+ *
+ * @author 王帅
+ * @since 2.0
+ */
+@RequiredArgsConstructor
+@Configuration(proxyBeanMethods = false)
+@EnableConfigurationProperties(XxlJobProperties.class)
+public class XxlJobConfig {
+
+    private final XxlJobProperties xxlJobProperties;
+
+    @Bean
+    public XxlJobSpringExecutor xxlJobExecutor() {
+        XxlJobSpringExecutor xxlJobSpringExecutor = new XxlJobSpringExecutor();
+        xxlJobSpringExecutor.setAccessToken(xxlJobProperties.getAccessToken());
+        xxlJobSpringExecutor.setAdminAddresses(xxlJobProperties.getAdminAddresses());
+        XxlJobProperties.Executor executor = xxlJobProperties.getExecutor();
+        xxlJobSpringExecutor.setIp(executor.getIp());
+        xxlJobSpringExecutor.setPort(executor.getPort());
+        xxlJobSpringExecutor.setAppname(executor.getAppName());
+        xxlJobSpringExecutor.setAddress(executor.getAddress());
+        xxlJobSpringExecutor.setLogPath(executor.getLogPath());
+        xxlJobSpringExecutor.setLogRetentionDays(executor.getLogRetentionDays());
+        return xxlJobSpringExecutor;
+    }
+
+}
